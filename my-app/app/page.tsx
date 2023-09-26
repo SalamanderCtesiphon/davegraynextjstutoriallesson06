3ba5be54b7581908b829a27d368db63167bcf075
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-26 17:39:47 -0500

    updated the home page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,14 @@
 
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Shannon</span>.
+        </span>
+      </p>
     </main>
   )
 }
