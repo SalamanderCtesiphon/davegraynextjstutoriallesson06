cd0b763c6e304374c2f1ee587952cba871729d34
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-26 18:48:00 -0500

    added a note for an idea of an upgrade to the application

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,3 +1,4 @@
+import Posts from "./components/Post"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
           I&apos;m <span className="font-bold">Shannon</span>.
         </span>
       </p>
+      <Posts />
     </main>
   )
 }
